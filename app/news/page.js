4066f7b894a2995d54dac7948e6b7a348fa775c3
fullengineer <fullengineer9@gmail.com>
2024-08-20 15:58:38 -0400

    Add latest news and archive news page.

--- app/news/page.js
+++ app/news/page.js
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import NewsList from '@/components/news-list';
 
 import { DUMMY_NEWS } from '@/dummy-news';
 
@@ -6,16 +6,7 @@ export default function NewsPage() {
   return (
     <>
       <h1>News Page</h1>
-      <ul className="news-list">
-        {DUMMY_NEWS.map(newsItem => (
-          <li key={newsItem.id}>
-            <Link href={`/news/${newsItem.slug}`}>
-              <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-              <span>{newsItem.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <NewsList news={DUMMY_NEWS} />
     </>
   );
 }
