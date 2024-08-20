fff6957afc3e537e72eefb811ddd166b4fea8ad4
fullengineer <fullengineer9@gmail.com>
2024-08-20 15:10:38 -0400

    Add Header component.

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import MainHeader from '@/components/main-header';
+
 import './globals.css';
 
 export const metadata = {
@@ -8,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MainHeader />
+        {children}
+      </body>
     </html>
   );
 }
