dc37519af78e44be91bb886e43eefa489214114a
fullengineer <fullengineer9@gmail.com>
2024-08-20 22:52:49 -0400

    Change fetching from db.

--- app/(content)/archive/@latest/default.js
+++ app/(content)/archive/@latest/default.js
@@ -2,8 +2,8 @@ import NewsList from '@/components/news-list';
 
 import { getLatestNews } from '@/lib/news';
 
-export default function LatestNewsPage() {
-  const latestNews = getLatestNews();
+export default async function LatestNewsPage() {
+  const latestNews = await getLatestNews();
 
   return (
     <>
